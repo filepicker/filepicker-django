c3af0964335ab9bce550a2a33e6588b3ee2a1dea
Jonas <jvp@jonasundderwolf.de>
2012-10-05 17:39:51 +0200

    Internationalize widget labels

--- django_filepicker/widgets.py
+++ django_filepicker/widgets.py
@@ -1,5 +1,8 @@
 from django.conf import settings
 from django.forms import widgets
+from django.utils.html import escape
+from django.utils.translation import ugettext as _
+
 
 #JS_URL is the url to the filepicker.io javascript library
 JS_VERSION = 0
@@ -14,6 +17,14 @@ else:
 class FPFileWidget(widgets.Input):
     input_type = INPUT_TYPE
     needs_multipart_form = False
+
+    def build_attrs(self, *args, **kwargs):
+        attrs = super(FPFileWidget, self).build_attrs(*args, **kwargs)
+        attrs.update({
+            'data-fp-button-text': escape(_('Pick File')),
+            'data-fp-drag-text': escape(_('Or drop files here')),
+        })
+        return attrs
 
     def value_from_datadict_old(self, data, files, name):
         #If we are using the middleware, then the data will already be
