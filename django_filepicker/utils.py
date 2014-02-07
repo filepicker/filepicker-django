196d36fb875f428290b4d7142b0b79ed82e7ec7a
Bryan <btbonval@gmail.com>
2014-02-07 01:19:56 -0500

    reworking param handling to support get_url and get_file independently.

--- django_filepicker/utils.py
+++ django_filepicker/utils.py
@@ -9,37 +9,77 @@ class FilepickerFile(File):
     filepicker_url_regex = re.compile(
             r'https?:\/\/www.filepicker.io\/api\/file\/.*')
 
-    def __init__(self, url):
-        if not self.filepicker_url_regex.match(url):
-            raise ValueError('Not a filepicker.io URL: %s' % url)
-        self.url = url
-
-    def get_file(self, additional_params={}):
+    def __init__(self, url, additional_params={}):
         '''
-        Downloads the file from filepicker.io and returns a
-        Django File wrapper object.
+        url should be a Filepicker URL pointing at a file reference.
+
         additional_params should include key/values such as:
         {
           'data-fp-signature': HEXDIGEST,
           'data-fp-policy': HEXDIGEST,
         }
-        (in other words, parameters should look like additional_params
-        of the models)
+        (it should look like additional_params of the models)
         '''
-        # clean up any old downloads that are still hanging around
-        self.cleanup()
+        if not self.filepicker_url_regex.match(url):
+            raise ValueError('Not a filepicker.io URL: %s' % url)
+        self.url = url
+        self.set_additional_params(additional_params)
+        # This helps Django File understand the File is not yet open.
+        self.name = ""
+
+    def set_additional_params(self, additional_params={}):
+        '''
+        Sets self.additional_params. Returns nothing.
+        If called with no arguments, self.additional_parms will be cleared.
+        '''
+        self.additional_params = additional_params
+
+    def get_url(self):
+        '''
+        Constructs the upstream Filepicker URL for reading/downloading on top
+        of self.url passed into the constructor.
+        '''
+        if not self.additional_params:
+            return self.url
 
         # Fetch any fields possibly required for fetching files for reading.
         query_params = {}
         for field in ('policy','signature'):
             longfield = 'data-fp-{0}'.format(field)
-            if longfield in additional_params:
-                query_params[field] = additional_params[longfield]
+            if longfield in self.additional_params:
+                query_params[field] = self.additional_params[longfield]
         # Append the fields as GET query parameters to the URL in data.
         query_params = urllib.urlencode(query_params)
         url = self.url
         if query_params:
             url = url + '?' + query_params
+        return url
+
+    def get_file(self, reset=True):
+        '''
+        Downloads the file from filepicker.io and returns a
+        Django File wrapper object.
+
+        If a file has been downloaded with an explicit call to get_file, run
+        cleanup to remove that file from the context. Otherwise get_file will
+        return the current file.
+
+        If get_file returns the current file as above, reset=True indicates that
+        the descriptor pointer should be reset to the beginning of the file.
+        reset=False indicates that the file pointer should be left as-is.
+        '''
+        if hasattr(self, 'file'):
+            if not self.file.closed:
+                # If there is a current file context, return self.
+                if reset:
+                    self.seek(0)
+                return self
+            else:
+                # Closed file means we might as well cleanup and get_file()
+                self.cleanup()
+
+        # construct a URL for reading from Filepicker
+        url = self.get_url()
 
         # The temporary file will be created in a directory set by the
         # environment (TEMP_DIR, TEMP or TMP)
@@ -73,6 +113,9 @@ class FilepickerFile(File):
             if os.path.exists(self.filename):
                 os.remove(self.filename)
             delattr(self, 'filename')
+
+        # This helps Django File understand the File is not yet open.
+        self.name = ""
 
     def __enter__(self):
         '''
